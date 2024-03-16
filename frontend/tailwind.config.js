43c25a57c0f00964461322e8b8979b393e84def1
Faruk Avcı <55799734+Faruk372742@users.noreply.github.com>
2024-03-16 16:33:10 +0300

    feat: button implementation

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        white: "#FFFFFF",
+        black: "#000000",
+        blue: "#40AFD2",
+        green: "#00C3C9",
+        lightGreen: "#D3ECEE",
+        red: "#FF6060",
+        darkGreen: "#016C81",
+      },
+      fontFamily: {
+        poppins: ["Poppins"],
+      },
     },
   },
   plugins: [],
