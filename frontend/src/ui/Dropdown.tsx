c043677a460a7571bf68f09bb0b59922e8cfe774
maqamylee0 <maqamylee0@gmail.com>
2024-03-20 15:00:00 +0300

    update dropdown cpt

--- frontend/src/ui/Dropdown.tsx
+++ frontend/src/ui/Dropdown.tsx
@@ -4,18 +4,32 @@ import { FaCaretDown, FaMinus } from "react-icons/fa";
 interface DropdownProps {
   title: string;
   options: string[];
+  value: string;
+  onChange: (value: string) => void;
 }
 
-const Dropdown: React.FC<DropdownProps> = ({ title, options }) => {
+const Dropdown: React.FC<DropdownProps> = ({
+  title,
+  options,
+  value,
+  onChange,
+}) => {
   const [isOpen, setIsOpen] = useState(false);
+
+  const handleOptionClick = (option: string) => {
+    onChange(option);
+    setIsOpen(false);
+  };
 
   return (
     <div className="relative">
       <button
         onClick={() => setIsOpen(!isOpen)}
-        className="justify-between border border-blue text-blue w-5/6 px-4 py-2 space-x-8 rounded flex items-center bg-transparent"
+        className={`justify-between border-4 border-darkGreen ${
+          isOpen ? "rounded-b-none" : "rounded-lg"
+        } text-darkGreen w-5/6 px-4 py-2 space-x-8 rounded-lg flex items-center bg-transparent`}
       >
-        {title}
+        {value || title}
         {isOpen ? (
           <FaMinus className="ml-2 " />
         ) : (
@@ -23,11 +37,12 @@ const Dropdown: React.FC<DropdownProps> = ({ title, options }) => {
         )}
       </button>
       {isOpen && (
-        <ul className="absolute left-0 mt-2 py-2 w-5/6 bg-white border border-blue rounded-lg shadow-xl">
+        <ul className="absolute left-0 mt- py-2 w-5/6 bg-white border-t-0 border-4 border-darkGreen rounded-t-none rounded-lg shadow-xl">
           {options.map((option, index) => (
             <li
               key={index}
-              className="px-4 py-2 hover:bg-blue hover:text-white"
+              className="px-4 py-2 hover:bg-lightGreen hover:text-darkGreen text-darkGreen"
+              onClick={() => handleOptionClick(option)}
             >
               {option}
             </li>
