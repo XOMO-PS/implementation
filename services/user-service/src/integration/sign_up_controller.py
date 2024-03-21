9954d35a16c2a4f6d8262ce7278bf22a61f5dab3
DidItMyWay <fathimasheikhn@gmail.com>
2024-03-21 22:01:21 +0100

    XP-31: adds user sign-in deployment configs

--- services/user-service/src/integration/sign_up_controller.py
+++ services/user-service/src/integration/sign_up_controller.py
@@ -1,40 +1,11 @@
-from flask import jsonify, request
 from flask import Flask
 import json
 
 from src.application.user_service import UserService
 from src.integration.model import user
 from src.integration.model import response_config
-from src.persistence import user_repository_impl
-
 
 app = Flask(__name__)
-
-def sign_in(event, context):
-    if 'body' in event and event['body']:
-        request_body = json.loads(event['body'])
-
-        try:
-            data = request_body
-            email = data.get('email')
-            password = data.get('password')
-    
-            if not email or not password:
-                return jsonify({'error': 'Missing credentials'}), 400
-
-            user = UserService.authenticate_user(email, password)
-            if user:
-                return jsonify({'status': 'Successfully signed in'}), 200
-            else:
-                return jsonify({'error': 'Invalid credentials'}), 401
-        
-        except Exception as e:
-            error_message = str(e)
-            operation_error = {
-                'statusCode': 500,
-                'body': json.dumps({'Error': f'{error_message}'})
-                }
-            return operation_error
 
 def registration_handler(event, context):
  
